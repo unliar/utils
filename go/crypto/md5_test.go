e582604694536bbd416b545eb9a3ca0712e3aedb
unliar <370732889@qq.com>
2018-12-12 00:31:18 +0800

    tests:crypto

---
+++ go/crypto/md5_test.go
@@ -0,0 +1,13 @@
+package crypto
+
+import "testing"
+import "reflect"
+
+func TestGetMD5(t *testing.T) {
+	s := "Hello,Golang!"
+	md := "9cea3f44c8955043a21997961b5f60a2"
+	if !reflect.DeepEqual(GetMD5(s), md) {
+		t.Errorf("o MD5Test failt,%s md5 should be %s ,but %s", s, md, GetMD5(s))
+	}
+
+}
