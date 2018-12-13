d9f464a85f8aa10fcb94f05e34d4f755f1469de8
unliar <370732889@qq.com>
2018-12-13 11:49:51 +0800

    feat:math random

--- go/math/math.go
+++ go/math/math.go
@@ -1,1 +1,8 @@
 package math
+
+import "math/rand"
+
+// GetRandomInt 生成max~min 随机整数
+func GetRandomInt(max int, min int) int {
+	return rand.Intn(max-min) + min
+}
--- go/math/math_test.go
+++ go/math/math_test.go
@@ -1,1 +1,13 @@
 package math
+
+import (
+	"testing"
+)
+
+func TestGetRandomInt(t *testing.T) {
+	max, min := 10, 1
+	r := GetRandomInt(max, min)
+	if r > max || r < min {
+		t.Error("out of range", r)
+	}
+}
