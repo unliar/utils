d7e38af0f75bd954ac662612c8d4d27b9a8e0abc
unliar <370732889@qq.com>
2018-12-19 00:22:46 +0800

    feat:md5 method can input more params

--- go/crypto/md5.go
+++ go/crypto/md5.go
@@ -4,7 +4,10 @@ import "fmt"
 import "crypto/md5"
 
 // GetMD5 return a md5 string of your input
-func GetMD5(s string) string {
-	d := []byte(s)
-	return fmt.Sprintf("%x", md5.Sum(d))
+func GetMD5(s ...string) string {
+	var d string
+	for _, item := range s {
+		d = d + item
+	}
+	return fmt.Sprintf("%x", md5.Sum([]byte(d)))
 }
--- go/crypto/md5_test.go
+++ go/crypto/md5_test.go
@@ -2,12 +2,21 @@ package crypto
 
 import "testing"
 import "reflect"
+
 // TestGetMD5 测试md5生成
 func TestGetMD5(t *testing.T) {
+	// single params
 	s := "Hello,Golang!"
 	md := "9cea3f44c8955043a21997961b5f60a2"
+
 	if !reflect.DeepEqual(GetMD5(s), md) {
 		t.Errorf("o MD5Test failt,%s md5 should be %s ,but %s", s, md, GetMD5(s))
 	}
+	// mutiple params
+	d := "Hello,Node"
+	mdd := "b5a19c344179a1943449630d98515236"
 
+	if !reflect.DeepEqual(GetMD5(s, d), mdd) {
+		t.Errorf("o MD5Test failt,%s md5 should be %s ,but %s", s, md, GetMD5(s))
+	}
 }
