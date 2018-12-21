d1649017cf6b3eafa680eb4515bebbd949d95d99
unliar <370732889@qq.com>
2018-12-21 11:19:25 +0800

    feat:node math

---
+++ node/math/index.js
@@ -0,0 +1,9 @@
+/**
+ * Get a random number between min ~ max
+ *
+ * @param {number} max
+ * @param {number} min
+ */
+const GetRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
